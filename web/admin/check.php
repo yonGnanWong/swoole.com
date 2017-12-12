2510c57321d95808f1ddccdcb71d88288eeb2697
matyhtf <mikan.tenny@gmail.com>
2017-12-12 11:12:40 +0800

    删除admin

--- web/admin/check.php
+++
@@ -1,9 +0,0 @@
-<?php
-$php->tpl->template_dir = WEBPATH.'/admin/templates';
-$php->session->start();
-Swoole\Auth::$session_prefix = 'admin_';
-Swoole\Auth::$login_url = '/admin/login.php?';
-Swoole\Auth::login_require();
-
-$access = array();
-require_once "../dict/acl.php";
